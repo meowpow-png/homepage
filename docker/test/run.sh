4e50b21f2fc2c5aa318d99ed18b1c78f5ab0c551
meowpow-png <marin.softic@proton.me>
2026-07-28 22:29:17 +0200

    Start running e2e tests in CI workflow

--- docker/test/run.sh
+++ docker/test/run.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-git config --global --add safe.directory /workspace &&
-npm ci &&
-npm run coverage &&
-npm run e2e &&
-npm run coverage:merge
+run_tests() {
+  npm run coverage &&
+  npm run e2e &&
+  npm run coverage:merge
+}
+
+git config --global --add safe.directory /workspace
+
+if [ "$1" = "--skip-install" ]; then
+  run_tests
+else
+  npm ci && run_tests
+fi
 status=$?
 
 chown -R "$(stat -c '%u:%g' /workspace)" tests/output 2>/dev/null
