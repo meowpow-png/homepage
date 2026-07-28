b9e4f89547484428626eebd003a0ddf65475e090
meowpow-png <marin.softic@proton.me>
2026-07-28 02:08:34 +0200

    Uninstall monocart-coverage dependency

--- tests/mcr.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    name: 'Combined Coverage Report',
-    outputDir: 'tests/output/coverage-combined',
-    reports: ['v8', 'raw'],
-    entryFilter: {
-        '**/src/**': true,
-    },
-}
