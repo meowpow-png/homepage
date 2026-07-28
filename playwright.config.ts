7692bb8aa2623b444bd646476303450e3581c0ab
meowpow-png <marin.softic@proton.me>
2026-07-28 02:28:31 +0200

    Start cleaning old test coverage data

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/e2e',
   outputDir: './tests/output/test-results',
+  globalSetup: './tests/e2e/globalSetup.ts',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
---
+++ tests/e2e/globalSetup.ts
@@ -0,0 +1,5 @@
+import { rm } from 'node:fs/promises'
+
+export default async function globalSetup(): Promise<void> {
+    await rm('tests/output/coverage-e2e', { recursive: true, force: true })
+}
