7c01f6d578f823699a44742536e9e22aa51c1360
meowpow-png <marin.softic@proton.me>
2026-07-28 04:52:04 +0200

    Stop linting css with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 import markdown from '@eslint/markdown'
-import css from '@eslint/css'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
@@ -40,5 +39,4 @@ export default defineConfig([
     language: 'markdown/gfm',
     extends: ['markdown/recommended'],
   },
-  { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
 ])
