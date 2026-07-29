af1bb362551d2dbae3a526e8966a0245a1984ba0
meowpow-png <marin.softic@proton.me>
2026-07-29 07:05:41 +0200

    Refine get-playwright-version script

--- docker/test/get-playwright-version.sh
+++ docker/test/get-playwright-version.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 set -eu
 
-version=$(node -p "require('./package-lock.json').packages['node_modules/@playwright/test'].version")
-
-echo "version=$version"
+node -p "require('./package-lock.json').packages['node_modules/@playwright/test'].version"
