e1e8a3bb9f525f1c5dccde2c569a575fcad8df5b
meowpow-png <marin.softic@proton.me>
2026-07-31 11:51:44 +0200

    Fix client discarding prerendered HTML

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { hydrateRoot } from 'react-dom/client'
 
 import '@/shared/styles/tokens.css'
 import '@/shared/styles/global.css'
@@ -7,7 +7,8 @@ import '@/shared/styles/typography.css'
 
 import { App } from './App'
 
-createRoot(document.getElementById('root')!).render(
+hydrateRoot(
+  document.getElementById('root')!,
   <StrictMode>
     <App />
   </StrictMode>,
