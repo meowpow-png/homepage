73daab4284fb4a2ef558d0c252b9fb2e87a09d2d
meowpow-png <marin.softic@proton.me>
2026-07-31 06:39:08 +0200

    Start generating lighthouse report index page

--- docker/lighthouse/run.sh
+++ docker/lighthouse/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-npm ci --ignore-scripts && npm run lighthouse
+npm ci --ignore-scripts && npm run lighthouse && npm run lighthouse:index
 status=$?
 
 chown -R "$(stat -c '%u:%g' /workspace)" dist .lighthouseci 2>/dev/null
