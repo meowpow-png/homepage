d50e81fb5266d27749884b2418d055052806e62d
meowpow-png <marin.softic@proton.me>
2026-08-01 20:16:20 +0200

    Suppress HTML usage warning

--- scripts/prerender.js
+++ scripts/prerender.js
@@ -46,6 +46,7 @@ export function buildSitemap(entries, siteUrl) {
     })
     .join('\n')
 
+  // noinspection HttpUrlsUsage
   return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>\n`
 }
 
